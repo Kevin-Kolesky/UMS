03da34c5dbe0d6af0d09b251df7ef8a755d2dc9c
Kevin-Kolesky <kevinkolesky03@gmail.com>
2024-05-14 22:01:21 +0200

    Table Styling

--- dashboard.js
+++ dashboard.js
@@ -71,6 +71,8 @@ function displayusers() {
             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
             </button>
+        </td>    
+        <td>
             <button type="button"  class="float-end delete-button danger-button btn-danger btn" aria-label="Close" id="edit${index}"
             data-bs-toggle="modal" data-bs-target="#deleteModal">
             <?xml version="1.0" ?>

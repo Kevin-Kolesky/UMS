e5a05ab8a64be2b0363615b23812e967de9a189a
Kevin-Kolesky <kevinkolesky03@gmail.com>
2024-05-18 11:26:12 +0200

    Table scroll implemented correctly

--- dashboard.js
+++ dashboard.js
@@ -64,16 +64,16 @@ function displayusers() {
         <td>${user.email}</td>
         <td>${user.password}</td>
         <td>                
-            <button type="button" class=" edit-button btn btn-primary float-start" id="edit${index}" 
+            <button type="button" class=" edit-button btn btn-primary" id="edit${index}" 
             data-bs-toggle="modal" data-bs-target="#editModal">
             <?xml version="1.0" ?>
-            <svg class="feather feather-edit" fill="none" height="20" stroke="currentColor" stroke-linecap="round" 
+            <svg class"feather feather-edit" fill="none" height="20" stroke="currentColor" stroke-linecap="round" 
             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
             </button>
         </td>    
         <td>
-            <button type="button"  class="float-end delete-button danger-button btn-danger btn" aria-label="Close" id="edit${index}"
+            <button type="button"  class="delete-button danger-button btn-danger btn" aria-label="Close" id="edit${index}"
             data-bs-toggle="modal" data-bs-target="#deleteModal">
             <?xml version="1.0" ?>
             <svg class="feather feather-trash" fill="none" height="20" stroke="currentColor" 
